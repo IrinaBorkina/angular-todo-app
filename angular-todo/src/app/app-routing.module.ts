442e22cbfde98980f06cadafc116c45df7f2c21e
Irina <irina_4ever@mail.ru>
2020-05-26 11:53:24 +0300

    feat: implement not found pages

--- angular-todo/src/app/app-routing.module.ts
+++ angular-todo/src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    component: NotFoundComponent,
+    loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule)
   },
 ];
 
--- angular-todo/src/app/app.module.ts
+++ angular-todo/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { AppTodoModule } from './todo/todo-app.module';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { TodoRoutingModule } from './todo/todo-routing.module';
 import { CanProceedToAboutGuard } from './guards/can-proceed-to-about.guard';
 
@@ -17,7 +14,6 @@ import { CanProceedToAboutGuard } from './guards/can-proceed-to-about.guard';
     AppComponent,
     HomeComponent,
     AboutComponent,
-    NotFoundComponent,
   ],
   imports: [
   BrowserModule,
